02a23dfc6340d935238389c7f88e0ba4c9e9290f
Tetra Puzzle <tetrapuzzle@gmail.com>
2021-01-25 03:33:02 +0330

    change version some fixes

--- Assets/Cafebazaar/PluginVersion.cs
+++ Assets/Cafebazaar/PluginVersion.cs
@@ -2,7 +2,7 @@ namespace CafeBazaar
 {
     public class PluginVersion
     {
-        public const int VersionInt = 4;
-        public const string VersionString = "0.0.4";
+        public const int VersionInt = 5;
+        public const string VersionString = "0.0.5";
     }
 }
