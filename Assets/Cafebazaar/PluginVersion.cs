0990825656afa1ce074295d776baeab7b263f298
Tetra Puzzle <tetrapuzzle@gmail.com>
2021-02-02 17:25:47 +0330

    Fix double purchase

--- Assets/Cafebazaar/PluginVersion.cs
+++ Assets/Cafebazaar/PluginVersion.cs
@@ -2,7 +2,7 @@ namespace CafeBazaar
 {
     public class PluginVersion
     {
-        public const int VersionInt = 5;
-        public const string VersionString = "0.0.5";
+        public const int VersionInt = 6;
+        public const string VersionString = "0.0.6";
     }
 }
